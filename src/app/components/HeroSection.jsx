d1ba0ac31b76cf0ea735ec0a264b589cfcdba9f2
Gabriela Mayumi <gabriela4matueda@gmail.com>
2025-01-09 23:03:08 -0300

    refactor: colors

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -6,11 +6,11 @@ import { TypeAnimation } from 'react-type-animation';
 
 const HeroSection = () => {
     return (
-        <section>
+        <section className="lg:py-16">
             <div className="grid grid-cols-1 sm:grid-cols-12">
-                <div className="col-span-7 place-self-center text-center sm:text-left">
+                <div className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
                     <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold">
-                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Hello, I'm{" "} 
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">Hello, I'm{" "} 
                         </span>
                         <br></br>
                         <TypeAnimation
@@ -33,13 +33,13 @@ const HeroSection = () => {
                         Lorem ipsum dolor sit amet. Cum molestiae nisi non maiores obcaecati ad quia soluta hic veritatis aperiam est quaerat facilis et voluptatem temporibus qui doloribus sint! Qui animi labore et nihil dolores et quaerat saepe qui facilis dignissimos ea quia impedit est libero fugiat.
                     </p> */}
                     <div>
-                        <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br  from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">Me contrate</button>
-                        <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white">
+                        <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br  from-blue-500 via-primary-500 to-secondary-500 hover:bg-slate-200 text-white">Me contrate</button>
+                        <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500 hover:bg-slate-800 text-white">
                             <span className="block bg-[#121212] hover:bg-slate-800  rounded-full px-5 py-2">Baixar CV</span>
                         </button>
                     </div>
                 </div>
-                <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+                <div className="col-span-4 place-self-center mt-4 lg:mt-0">
                     <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
                         <Image
                         src="/images/hero-image.png"
