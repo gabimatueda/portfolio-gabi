e6dc9d5c5c10df4715ea33845579336da6179896
Gabriela Mayumi <gabriela4matueda@gmail.com>
2025-01-14 11:32:43 -0300

    feat: experience section

--- src/app/page.js
+++ src/app/page.js
@@ -5,16 +5,17 @@ import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
-// import AchievimentsSection from "./components/AchievementsSection";
+import WorkExperiences from "./components/WorkExperiences";
 
 export default function Home() {
   return (
       <main className="flex min-h-screen flex-col bg-[#121212]">
         <Navbar />
-        <div class="container mt-24 mx-auto px-12 py-4">
+        <div className="container mt-24 mx-auto px-12 py-4">
           <HeroSection />
           <AboutSection/>
           <ProjectsSection/>
+          <WorkExperiences/>
           <EmailSection/>
         </div>
         <Footer/>
